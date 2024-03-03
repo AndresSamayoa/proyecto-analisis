c63b6245aca4c0b30f1be3c959b641924595b422
Andres Samayoa Penagos <andressamayoap@gmail.com>
2024-03-03 16:29:12 -0600

    feat(add): header layout and its use in app

---
+++ src/Layouts/Header/Header.js
@@ -0,0 +1,16 @@
+import "./Header.css";
+
+function Header() {
+    return <div className="Header">
+        <div className="Title">
+            <p>Cuentas por cobrar</p>
+        </div>
+        <div className="Options">
+            <p>Link 1</p>
+            <p>Link 2</p>
+            <p>Link 3</p>
+        </div>
+    </div>
+}
+
+export default Header;
