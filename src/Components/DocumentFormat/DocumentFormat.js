79647b2e183dcae9468180cf101ddbd8ff089139
Andres Samayoa Penagos <andressamayoap@gmail.com>
2024-05-16 08:57:35 -0600

    feat(add): api usage in DocumentFormat

--- src/Components/DocumentFormat/DocumentFormat.js
+++ src/Components/DocumentFormat/DocumentFormat.js
@@ -2,11 +2,15 @@ import './DocumentFormat.css';
 import logo from '../../Assets/Logo.png';
 
 import axios from 'axios';
+import XMLParser from 'react-xml-parser';
+import querystring from 'querystring';
 import * as moment from 'moment';
 import { usePDF } from 'react-to-pdf';
 import { useState } from 'react';
 
-function DocumentFormat(props) {
+const net_base_url = process.env.REACT_APP_DOT_NET_API_BASE;
+
+function DocumentFormat() {
     const {toPDF, targetRef} = usePDF({filename: `Factura(${moment().format('DD-MM-YYYY hh:mm')}).pdf`});
 
     const [mensajeTabla, setMensajeTabla] = useState('');
@@ -20,8 +24,28 @@ function DocumentFormat(props) {
     const [numero, setNumero] = useState('');
     const [numeroTransmision, setNumeroTransmision] = useState('');
     const [fechaTransmision, setFechaTransmision] = useState('');
+    const [fechaAnulacion, setFechaAnulacion] = useState('');
+    const [tipoDocumento, setTipoDocumento] = useState('');
     const [total, setTotal] = useState(0);
     const [tableData, setTableData] = useState([]);
+
+    const clearForm = () => {
+        setMensajeTabla('');
+        setParam('');
+        setCliente('');
+        setDireccion('');
+        setTelefono('');
+        setEmail('');
+        setNit('');
+        setSerie('');
+        setNumero('');
+        setNumeroTransmision('');
+        setFechaTransmision('');
+        setFechaAnulacion('');
+        setTipoDocumento('');
+        setTotal(0);
+        setTableData([]);
+    }
 
     const formatItems = (items) => {
         const tempData = [];
@@ -41,13 +65,57 @@ function DocumentFormat(props) {
 
     const getData = async () => {
         try {
+            clearForm();
+            const respuesta = await axios({
+                method: 'POST',
+                url: net_base_url+'/CXC_Documento.asmx/Encabezado_documento',
+                data: querystring.stringify({
+                    iddocumento: param,
+                }),
+                validateStatus: status => true
+            })
+
+            if (respuesta.status >= 200 && respuesta.status < 300) {
+                const data = new XMLParser().parseFromString(respuesta.data)
+                if(data.children[1].children.length < 1) {
+                    setMensajeTabla('Documento no encontrado');
+                    return;
+                };
+
+                const itemHeader = data.children[1].children[0].children[0];
+
+                setCliente(itemHeader.children.find(obj => obj.name === 'RAZON_SOCIAL_CLIENTE') ? itemHeader.children.find(obj => obj.name === 'RAZON_SOCIAL_CLIENTE').value : null);
+                setDireccion(itemHeader.children.find(obj => obj.name === 'DIRECCION_CLIENTE') ? itemHeader.children.find(obj => obj.name === 'DIRECCION_CLIENTE').value : null);
+                setTelefono(itemHeader.children.find(obj => obj.name === 'TELEFONO_CLIENTE') ? itemHeader.children.find(obj => obj.name === 'TELEFONO_CLIENTE').value : null);
+                setEmail(itemHeader.children.find(obj => obj.name === 'EMAIL_CLIENTE') ? itemHeader.children.find(obj => obj.name === 'EMAIL_CLIENTE').value : null);
+                setNit(itemHeader.children.find(obj => obj.name === 'NIT_CLIENTE') ? itemHeader.children.find(obj => obj.name === 'NIT_CLIENTE').value : null);
+                setSerie(itemHeader.children.find(obj => obj.name === 'SERIE_DOCUMENTO') ? itemHeader.children.find(obj => obj.name === 'SERIE_DOCUMENTO').value : null);
+                setNumero(itemHeader.children.find(obj => obj.name === 'NUMERO_DOCUMENTO') ? itemHeader.children.find(obj => obj.name === 'NUMERO_DOCUMENTO').value : null);
+                setNumeroTransmision(itemHeader.children.find(obj => obj.name === 'NUMERO_EMISION_DOCUMENTO') ? itemHeader.children.find(obj => obj.name === 'NUMERO_EMISION_DOCUMENTO').value : null);
+                setFechaTransmision(itemHeader.children.find(obj => obj.name === 'FECHA_EMISION_DOCUMENTO') ? itemHeader.children.find(obj => obj.name === 'FECHA_EMISION_DOCUMENTO').value : null);
+                setTotal(itemHeader.children.find(obj => obj.name === 'VALOR_DOCUMENTO') ? itemHeader.children.find(obj => obj.name === 'VALOR_DOCUMENTO').value : null);
+                setFechaAnulacion(itemHeader.children.find(obj => obj.name === 'FECHA_ANULACION_DOCUMENTO') ? itemHeader.children.find(obj => obj.name === 'FECHA_ANULACION_DOCUMENTO').value : null);
+                setTipoDocumento(itemHeader.children.find(obj => obj.name === 'TIPO_DOCUMENTO') ? itemHeader.children.find(obj => obj.name === 'TIPO_DOCUMENTO').value : null);
+
+                getDetails();
+
+            } else {
+                setMensajeTabla('Error ' + respuesta.status + ': ' + respuesta.data);
+            }
+        } catch (error) {
+            setMensajeTabla('Error: ' + error.message);
+        }
+    }
+
+    const getDetails = async () => {
+        try {
             setTableData([]);
             const respuesta = await axios({
                 method: 'POST',
-                url: net_base_url+'/CXC_Documento.asmx/consultarDoc',
-                data: {
-                    pDocumento: param,
-                },
+                url: net_base_url+'/CXC_Documento.asmx/detalle_documento',
+                data: querystring.stringify({
+                    iddocumento: param,
+                }),
                 validateStatus: status => true
             })
 
@@ -55,9 +123,24 @@ function DocumentFormat(props) {
                 const data = new XMLParser().parseFromString(respuesta.data)
                 const tempData = [];
                 if(data.children[1].children.length < 1) {
+                    setMensajeTabla('Documento no encontrado');
                     return;
                 };
-                getRows(tempData);
+
+                const items = data.children[1].children[0];
+
+                for (const item of items.children) {
+                    tempData.push({
+                        cantidad: item.children.find(obj => obj.name === 'CANTIDAD') ? item.children.find(obj => obj.name === 'CANTIDAD').value : null,
+                        nombre_producto: item.children.find(obj => obj.name === 'DESCRIPCION') ? item.children.find(obj => obj.name === 'DESCRIPCION').value : null,
+                        precio_unitario: item.children.find(obj => obj.name === 'PRECIO') ? item.children.find(obj => obj.name === 'PRECIO').value : null,
+                        descuento: item.children.find(obj => obj.name === 'DESCUENTO') ? item.children.find(obj => obj.name === 'DESCUENTO').value : null,
+                        total: item.children.find(obj => obj.name === 'SUBTOTAL') ? item.children.find(obj => obj.name === 'SUBTOTAL').value : null,
+                    })   
+                }
+
+                formatItems(tempData);
+
             } else {
                 setMensajeTabla('Error ' + respuesta.status + ': ' + respuesta.data);
             }
@@ -87,7 +170,7 @@ function DocumentFormat(props) {
                             <h2>correo@mail.com</h2>
                         </div>
                         <div className='headerDocumentItem'>
-                            <h1>Factura</h1>
+                            <h1>{tipoDocumento.toUpperCase()}</h1>
                             <img className='imagelogo' src={logo} alt='cxc_logo'/>
                         </div>
                     </div>
@@ -104,7 +187,8 @@ function DocumentFormat(props) {
                             <h2>Serie: {serie}</h2>
                             <h2>Numero: {numero}</h2>
                             <h2>UUID: {numeroTransmision}</h2>
-                            <h2>Fecha: {fechaTransmision}</h2>
+                            <h2>Fecha: {fechaTransmision ? moment(fechaTransmision).format('DD-MM-YYYY') : ''}</h2>
+                            {fechaAnulacion && <h2>Fecha de anulacion: {fechaAnulacion ? moment(fechaAnulacion).format('DD-MM-YYYY') : ''}</h2>}
                         </div>
                     </div>
                     <div className="tableDocumentContainer">
--- src/Layouts/Reports/Reports.js
+++ src/Layouts/Reports/Reports.js
@@ -18,30 +18,7 @@ function Reports () {
             <GeneralCXCReport />
         }
         {report === 1 &&
-            <DocumentFormat 
-                cliente="CLIENTE OG"
-                direccion="3 ave 3-33 zona 3 Guatemala"
-                telefono="12345678"
-                email="cliente@mail.com"
-                serie="12"
-                numero="236342"
-                numero_transmision="abc-123-def-456"
-                fecha="20/10/2023"
-                total="20"
-                items={[{
-                        cantidad: 2,
-                        nombre_producto: 'TEST 1',
-                        precio_unitario: 5,
-                        descuento: 0,
-                        total: 10,
-                    },{
-                        cantidad: 2,
-                        nombre_producto: 'TEST 2',
-                        precio_unitario: 5,
-                        descuento: 0,
-                        total: 10,
-                }]}
-            />
+            <DocumentFormat />
         }
     </div>
 }
